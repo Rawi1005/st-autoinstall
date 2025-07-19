b03043e9f4ca17c7cbc4f5e9f3676ed3b1538577
Rawi1005 <rawinakasem@gmail.com>
2025-07-19 15:25:49 +0700

    Create skillissue.sh

---
+++ skillissue.sh
@@ -0,0 +1,11 @@
+echo -e "\n--- Node version ---"
+node -v
+echo -e "\n--- NPM version ---"
+npm -v
+echo -e "\n--- Free memory ---"
+free -m
+echo -e "\n--- Termux info ---"
+termux-info
+echo -e "\n--- Disk space ---"
+df -h ~/
+echo -e "\n--- End of check ---"

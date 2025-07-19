2103b0b064df1306b7518b5eec6d3af74c696d3e
Rawi1005 <rawinakasem@gmail.com>
2025-07-19 15:26:59 +0700

    Update skillissue.sh

--- skillissue.sh
+++ skillissue.sh
@@ -1,11 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
 echo -e "\n--- Node version ---"
 node -v
+
 echo -e "\n--- NPM version ---"
 npm -v
-echo -e "\n--- Free memory ---"
+
+echo -e "\n--- Free memory (in MB) ---"
 free -m
+
 echo -e "\n--- Termux info ---"
 termux-info
-echo -e "\n--- Disk space ---"
+
+echo -e "\n--- Disk space for home directory ---"
 df -h ~/
+
 echo -e "\n--- End of check ---"
